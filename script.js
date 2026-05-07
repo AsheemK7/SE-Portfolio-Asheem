0d04e91fb034d73e3451513b836cf90249ddd94e
Asheem Khan <heyasheemk7@gmail.com>
2026-05-08 00:31:31 +0530

    added cursor glow effect

--- script.js
+++ script.js
@@ -61,3 +61,13 @@ function eraseText(){
 }
 
 window.onload = typeWriter;
+/* CURSOR GLOW EFFECT */
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+
+});
